5b8560824a3685d27f5261761ab4ac74d8bad787
eduardocf2003 <florianoeduardo03@gmail.com>
2024-05-18 18:40:39 -0300

    Final Big O

---
+++ Big O/Resume.py
@@ -0,0 +1,22 @@
+#Resumo
+
+#O(n²) = Loop dentro de um Loop. (Custo de processamento muito alto).
+
+#O(n) = Proporcional a n, o numero de operações é sempre igual a n (Custo de processamento alto).
+
+#O(log n) = Dividir para conquistar (Custo de processamento Baixo)
+
+#O(1) = Constante (Custo de processamento muito baixo)
+
+#Para consultar o big o de qualquer algorítmo ou estrutura de dados, https://www.bigocheatsheet.com/.
+
+#Os alogritmos podem ter suas complexidades medidas em dois tipos, Complexidade de tempo e complexidade de espaço.
+
+#Quando observamos a complexidade de tempo, analisamos a relação entre o tamanho da entrada e o tempo de execução
+#do algoritmo.
+
+#Quando observamos a complexidade de espaço, analisamos a relação entre o tamanho da entrada e a quantidade de 
+#memória utilizada pelo algoritmo.
+
+#Vale lembrar que é perfeitamente possível o mesmo algoritmo possuir um péssimo desempenho de tempo,
+#mas um excelente desempenho quando analisamos a complexidade de espaço.
